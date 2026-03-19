d76f7df68297b9094cf8033ddb7054bc7d51b929
Phantomwise <phantomwise67@protonmail.com>
2026-03-19 01:33:47 +0100

    🚧 Define data types for card suit and rank

--- scouba.hs
+++ scouba.hs
@@ -2,6 +2,15 @@
 import Data.Csv
 
 
+-- Define data type for card suit
+data Suit = Spades | Hearts | Clubs | Diamonds
+    deriving (Eq, Enum, Show)
+
+-- Define data type for card rank
+data Rank = One | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
+    deriving (Eq, Enum, Ord, Bounded, Show)
+
+
 -- Make a record for cards
 data Card = Card
     { nm :: String
