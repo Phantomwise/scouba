69eaf961f5f5866a90e5b06c7e9d7bbea11f1d8a
Phantomwise <phantomwise67@protonmail.com>
2026-07-27 05:55:25 +0200

    ♻️ Move configuration to the Config module

--- src/Unsorted.hs
+++ src/Unsorted.hs
@@ -41,36 +41,11 @@ import qualified Data.ByteString.Char8 as B8
 
 
 -- ================================================================
--- CONFIGURATION
+-- LOCAL IMPORTS
 -- ================================================================
 
 
--- Enable/disable debug messages
-debug :: Bool
-debug = True
-
-
--- Deck File Path
-deckFilePath :: FilePath
-deckFilePath = "scouba_deck.csv"
-
-
--- Number of players
-playersCount :: Int
-playersCount = 2
--- Not yet in use
-
-
--- Number of cards in the hand
-handSize :: Int
-handSize = 5
--- Not yet in use
-
-
--- Number of cards on the table
-tableSize :: Int
-tableSize = 4
--- Not yet in use
+import Config
 
 
 -- ================================================================
